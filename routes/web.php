91d34d16b1979ae29dce910c9beb7bcf71426cf1
Sanni, Olanrewaju <slanre26@gmail.com>
2025-02-13 23:28:06 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// routes/web.php
+
+use Illuminate\Support\Facades\Env;
+
+// Define a route to dump .env variables
+Route::get('/env', function () {
+    // Dump all .env variables
+    dd([
+        'PAYSTACK_SECRET_KEY' => env('PAYSTACK_SECRET_KEY'),
+        'PAYSTACK_PAYMENT_URL' => env('PAYSTACK_PAYMENT_URL'),
+        'Config PAYSTACK_SECRET_KEY' => config('services.paystack.secret_key'),
+        'Config PAYSTACK_PAYMENT_URL' => config('services.paystack.payment_url'),
+    ]);
+});
