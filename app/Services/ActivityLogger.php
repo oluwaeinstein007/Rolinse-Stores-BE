f0229751f37ca3c7f587f1063b04024bba716069
Habeeb Lawal Temitope <habeeblawaltemitope@gmail.com>
2025-11-27 12:14:04 +0100

    Resolved gemini review for Little modification to paypal payment gateway

--- app/Services/ActivityLogger.php
+++ app/Services/ActivityLogger.php
@@ -6,11 +6,11 @@ use App\Models\ActivityLog;
 
 class ActivityLogger
 {
-    public static function log($type, $action, $description, $user_id, $extraInfo = null)
+       public static function log($type, $action, $description, $user_id, $extraInfo = null)
     {
         ActivityLog::create([
             'type' => $type,
-            'sub_type' => $action,
+            'sub_type' => $action,  
             'action' => $action,
             'description' => $description,
             'user_id' => $user_id,
